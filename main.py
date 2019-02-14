39e6327798a4fb39c520594ef8dbc9c78204140a
Dmitry <dmitry@unipat.ru>
2019-02-14 06:35:45 +0300

    Dynamicly showing points from data.json

--- main.py
+++ main.py
@@ -11,51 +11,20 @@ def hello():
 @app.route("/api")
 def api():
     callback_function = request.args.get('callback')
+    coordinates = request.args.get('bbox')
+    x_min, y_min, x_max, y_max = coordinates.split(',')
+    x_min, y_min, x_max, y_max = float(x_min), float(y_min), float(x_max), float(y_max)
+    with open('data.json', 'r') as fp:
+        raw_data = loads(fp.read())
     data = {
         "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "id": 0,
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [55.831903, 37.411961]
-                },
-                "properties": {
-                    "balloonContent": "Содержимое балуна",
-                    "clusterCaption": "Метка 1",
-                    "hintContent": "Текст подсказки 1"
-                }
-            },
-            {
-                "type": "Feature",
-                "id": 1,
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [55.763338, 37.565466]
-                },
-                "properties": {
-                    "balloonContent": "Содержимое балуна",
-                    "clusterCaption": "Метка 2",
-                    "hintContent": "Текст подсказки 2"
-                }
-            },
-            {
-                "type": "Feature",
-                "id": 2,
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [56.000000, 37.311961]
-                },
-                "properties": {
-                    "balloonContent": "Содержимое балуна",
-                    "clusterCaption": "Метка 0",
-                    "hintContent": "Текст подсказки 0"
-                }
-            }
-
-        ]
+        "features": []
     }
+    for item in raw_data['features']:
+        if x_min < item["geometry"]["coordinates"][0] < x_max:
+            if y_min < item["geometry"]["coordinates"][1] < y_max:
+                data['features'].append(item)
+    print('points:', len(data['features']))
     json_data = dumps(data)
     response = f'{callback_function}({json_data})'
     return response
